746bb1c4d9a4a89fcb80e88976881c336b50fa72
Ranjodh <ranjodhsingh24@gmail.com>
2026-01-11 19:13:27 -0500

    Add function to fetch data from AWS API

---
+++ index.js
@@ -0,0 +1,11 @@
+export function callAwsData(){
+    fetch('https://kpyupsvpue.execute-api.us-east-1.amazonaws.com/dev/hello')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById('aws-data').innerText = data.body;
+    })
+    .catch(error => {
+        console.error('Error fetching data from AWS:', error);
+    });
+}
