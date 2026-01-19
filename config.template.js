4991acc9ed75882604e15c7df64c61a759d400cf
Ranjodh <ranjodhsingh24@gmail.com>
2026-01-18 23:46:22 -0500

    Fix typo in logoutUri property

--- config.template.js
+++ config.template.js
@@ -2,6 +2,6 @@ window.APP_CONFIG = {
   cognitoDomain : "__COGNITO_DOMAIN__",
   clientId : "__COGNITO_CLIENT_ID__",
   redirectUri : "__COGNITO_REDIRECT_URI__",
-  logutUril : "__COGNITO_LOGOUT_URI__",
+  logoutUri : "__COGNITO_LOGOUT_URI__",
   helloEndpoint : "__HELLO_ENDPOINT__"
 }
