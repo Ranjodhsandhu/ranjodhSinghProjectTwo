f6b836f4bc0fc9ec536263f448cbfc986bfc7687
Ranjodh <ranjodhsingh24@gmail.com>
2026-01-18 22:53:06 -0500

    Add APP_CONFIG template for application configuration

---
+++ config.template.js
@@ -0,0 +1,7 @@
+window.APP_CONFIG = {
+  cognitoDomain = "__COGNITO_DOMAIN__",
+  clientId = "__COGNITO_CLIENT_ID__",
+  redirectUri = "__COGNITO_REDIRECT_URI__",
+  logutUril = "__COGNITO_LOGOUT_URI__",
+  helloEndpoint = "__HELLO_ENDPOINT__"
+}
