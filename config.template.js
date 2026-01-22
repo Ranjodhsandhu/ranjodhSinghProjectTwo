03b95fb35cdb885c2201a96b9e6dec87caa824ad
Ranjodh <ranjodhsingh24@gmail.com>
2026-01-22 15:00:34 -0500

    add recipe endpoint to config

--- config.template.js
+++ config.template.js
@@ -3,5 +3,6 @@ window.APP_CONFIG = {
   clientId : "__COGNITO_CLIENT_ID__",
   redirectUri : "__COGNITO_REDIRECT_URI__",
   logoutUri : "__COGNITO_LOGOUT_URI__",
-  helloEndpoint : "__HELLO_ENDPOINT__"
+  helloEndpoint : "__HELLO_ENDPOINT__",
+  recipeEndpoint: "__RECIPE_ENDPOINT__"
 }
